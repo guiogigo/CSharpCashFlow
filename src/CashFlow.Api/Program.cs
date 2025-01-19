6914cc83062620225597e2ef013aed1434434f80
guiogigo <guilhermevbatista20@gmail.com>
2025-01-19 01:52:56 -0300

    Inicio Testes de Integração

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using CashFlow.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -84,7 +85,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await MigrateDatabase();
+if(builder.Configuration.IsTestEnvironment() == false)
+{
+    await MigrateDatabase();
+}
 
 app.Run();
 
--- tests/CommonTestUtilities/Entities/UserBuilder.cs
+++ tests/CommonTestUtilities/Entities/UserBuilder.cs
@@ -14,7 +14,7 @@ public class UserBuilder
             .RuleFor(u => u.Name, faker => faker.Person.FirstName)
             .RuleFor(u => u.Email, (faker, user) => faker.Internet.Email(user.Name))
             .RuleFor(u => u.Password, (_, user) => passwordEncripter.Encrypt(user.Password))
-            .RuleFor(u => u.UserIdentifier, _ => Guid.NewGuid());
+            .RuleFor(u => u.UserIdentify, _ => Guid.NewGuid());
 
         return user;
     }

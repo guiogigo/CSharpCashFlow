a4a85146d32fc81ce68eeb7bccd6ed1916bc9255
guiogigo <guilhermevbatista20@gmail.com>
2025-01-21 20:40:59 -0300

    Tag Migration

---
+++ src/CashFlow.Domain/Entities/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Domain.Entities;
+public class Tag
+{
+    public long Id { get; set; }
+    public Enums.Tag.Tag Value { get; set; }
+    public long ExpenseId { get; set; }
+    public Expense Expense { get; set; } = default!;
+}
--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -4,8 +4,14 @@ using Microsoft.EntityFrameworkCore;
 namespace CashFlow.Infrastructure.DataAccess;
 public class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
 
-    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
